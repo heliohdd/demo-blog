a45a045e1201190e2f171bb86353b4c8905e9728
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-15 19:55:46 -0300

    Adicionando componente de navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 // import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Navbar from "./pages/components/Navbar";
+import Footer from "./pages/components/Footer";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/demo-blog" element={<Home /> } />
-            <Route path="/demo-blog/about" element={<About /> } />
+            <Route path="/" element={<Home /> } />
+            <Route path="/about" element={<About /> } />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/pages/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavLink to={"/"}>
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to={"/"}>Home</NavLink>{" "}
+        </li>
+        <li>
+          <NavLink to={"/about"}>About</NavLink>{" "}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
