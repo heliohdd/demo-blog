87c91a9a38f0851a8a3fc031bc78f3584db7686d
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-16 12:37:42 -0300

    Estilos gerais do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Navbar from "./components/Navbar";
