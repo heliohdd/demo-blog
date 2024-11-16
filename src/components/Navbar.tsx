c6960b740a5a35953ea4737d0fdb3a1641e0035b
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-16 12:54:32 -0300

    Criando formulario de cadastro

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,13 @@ const Navbar = () => {
           <NavLink to={"/"} className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
         </li>
         <li>
-          <NavLink to={"/about"} className={({isActive}) => (isActive ? styles.active : '')}>About</NavLink>
+          <NavLink to={"/login"} className={({isActive}) => (isActive ? styles.active : '')}>Entrar</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/register"} className={({isActive}) => (isActive ? styles.active : '')}>Cadastrar</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/about"} className={({isActive}) => (isActive ? styles.active : '')}>Sobre</NavLink>
         </li>
       </ul>
     </nav>
